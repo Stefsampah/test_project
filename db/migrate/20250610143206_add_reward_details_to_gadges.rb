1cbc03644724ebfbe78e79efc4693d6347cd371e
stephane sampah gh api stefsampah@hotmail.com <stefsampah@hotmail.com>
2025-06-11 16:34:14 +0200

    reward cards formats and badges points and view

--- db/migrate/20250610143206_add_reward_details_to_gadges.rb
+++
@@ -1,6 +0,0 @@
-class AddRewardDetailsToGadges < ActiveRecord::Migration[7.1]
-  def change
-    add_column :gadges, :reward_description, :string
-    add_column :gadges, :image, :string
-  end
-end
