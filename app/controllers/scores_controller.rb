b4e09e41379b5e289b037a0afe1e0a172508021f
stephane sampah gh api stefsampah@hotmail.com <stefsampah@hotmail.com>
2025-11-27 15:31:28 +0100

    Add avatars to leaderboards in special categories section

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -10,6 +10,13 @@ class ScoresController < ApplicationController
     @best_ratio_scores = Score.calculate_best_ratio_scores.first(10)
     @wise_critic_scores = Score.calculate_wise_critic_scores.first(10)
     @total_scores = Score.calculate_total_scores.first(10)
+    
+    # Précharger tous les utilisateurs avec leurs avatars pour éviter les requêtes N+1
+    user_ids = (@top_engager_scores.map { |s| s[:user_id] } + 
+                @best_ratio_scores.map { |s| s[:user_id] } + 
+                @wise_critic_scores.map { |s| s[:user_id] } + 
+                @total_scores.map { |s| s[:user_id] }).uniq
+    @users_by_id = User.includes(avatar_attachment: :blob).where(id: user_ids).index_by(&:id)
 
     # Récupérer seulement les playlists avec des scores - LIMITÉES aux 5 plus populaires
     @scores_by_playlist = Playlist.includes(scores: :user)
