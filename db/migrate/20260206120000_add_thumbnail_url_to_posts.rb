495ebf7344f7445b8c878ee493ef58b1b56d2e1d
stephane sampah gh api stefsampah@hotmail.com <stefsampah@hotmail.com>
2026-02-06 17:07:50 +0100

    Make thumbnail_url migration idempotent

--- db/migrate/20260206120000_add_thumbnail_url_to_posts.rb
+++ db/migrate/20260206120000_add_thumbnail_url_to_posts.rb
@@ -1,5 +1,7 @@
 class AddThumbnailUrlToPosts < ActiveRecord::Migration[7.1]
   def change
+    return if column_exists?(:posts, :thumbnail_url)
+
     add_column :posts, :thumbnail_url, :string
   end
 end
